748036f3afe6464ad3fceeb74966fe8335711ca7
Hennechart <joelhen@hotmail.fr>
2023-09-06 13:56:45 +0200

    mise en place BDD + boutons articles

--- model/db_connexion.php
+++ model/db_connexion.php
@@ -2,15 +2,15 @@
 
 // Création d'une instance PDO (Php Data Object) pour se connecter à ma bdd: mapropositionvalphp//
 
-$dsn ="mysql:host=localhost;dbname=mapropositionvalphp;charrset=utf8";
+$dsn ="mysql:host=localhost;dbname=mapropositionvalphp;charrset=utf8"; //chemin veers la BDD//
 $user= "root";
 $password= "root";
 try{
     //si tout va bien//
     $db_connexion = new PDO($dsn, $user,$password);
     $db_connexion->setAttribute(PDO::MYSQL_ATTR_INIT_COMMAND, 'SET NAMES utf8');
-    $db_connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-    $db_connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $db_connexion->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC); //gerer le tableau associatif de la BDD
+    $db_connexion->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); //gerer les erreurs
     // :: veut dire l'enfant depend de son parent est recupére quelque chose de son parent 
 }
 catch(PDOException $e){
